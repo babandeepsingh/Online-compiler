d3d5a74f9ea812848fbd78ca9ec385e607ba5c0f
Babandeep Singh <babandeepsingh@gmail.com>
2024-08-17 15:51:54 +0530

    Updated files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Box } from "@chakra-ui/react";
+import './App.css'
+import CodeEditor from './components/CodeEditor';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+    <CodeEditor />
+
+    {/* hehre */}
+  </Box>
+  )
+}
+
+export default App
